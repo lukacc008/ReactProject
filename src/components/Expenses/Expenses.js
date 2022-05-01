133d3f927aa3a1008b5871a0658ca74323420dd5
unknown <lukacc008@gmail.com>
2022-05-01 15:09:14 +0200

    Added the map method to create expenses instead of hardcode

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,30 +15,18 @@ function Expenses(props) {
     return (
       <div>
    <Card className="expenses">
-   <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      <ExpenseItem 
-                   title={props.item[0].title}
-                   amount={props.item[0].amount}
-                   date={props.item[0].date} 
-      />
+   <ExpensesFilter 
+   selected={filteredYear} 
+   onChangeFilter={filterChangeHandler}
+   />
 
-      <ExpenseItem 
-                   title={props.item[1].title}
-                   amount={props.item[1].amount}
-                   date={props.item[1].date} 
-      />
-
-      <ExpenseItem 
-                   title={props.item[2].title}
-                   amount={props.item[2].amount}
-                   date={props.item[2].date} 
-      />
-
-      <ExpenseItem 
-                   title={props.item[3].title}
-                   amount={props.item[3].amount}
-                   date={props.item[3].date} 
-      />
+      {props.item.map((expense) => (
+      <ExpenseItem
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date}
+        />
+        ))}        
       </Card>
       </div>
     )
